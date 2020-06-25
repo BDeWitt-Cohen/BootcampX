28e3abc054299a6e07946a41f2155a4b4c0c4c53
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-25 14:10:07 +0000

    Updated the JS queries to be parametized to avoid malicious code and SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,24 +8,45 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;
-`)
-  .then(res => {
-    res.rows.forEach(teacher => {
-      console.log(`${teacher.cohort}: ${teacher.teacher}`);
-    })
+
+// **Parametized was of the bottom code, always do it this way when accepting inputs**
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
   })
-  .catch(err => console.error('query error', err.stack));
+})
+.catch(err => console.error('query error', err.stack));
 
 
 
-  // res.rows.forEach(user => {
-  //   console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  // })
+
+//  **Not a good way to do it as it will accept malicious code**
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// ORDER BY teacher;
+// `)
+//   .then(res => {
+//     res.rows.forEach(teacher => {
+//       console.log(`${teacher.cohort}: ${teacher.teacher}`);
+//     })
+//   })
+//   .catch(err => console.error('query error', err.stack));
